0bcf3876636df3d18792b9534ac060dad0988e4f
GrzLo <251583@uwr.edu.pl>
2019-03-07 13:21:37 +0100

    Update zad3.cpp

--- lista2/zad3.cpp
+++ lista2/zad3.cpp
@@ -2,18 +2,16 @@
 
 static int counter = 0;
 
+// n(n+1)/2 - 1
 int skalar(int a[], int b[], int n)
 {
 	int result = 0;
-	int counter = 0;
 	
 	for(int i = 0; i < n; ++i)
 	{
 		result += a[i] * b[i];
 		++counter;
 	}
-	
-	std::cout << "Number of multiplications: " << counter << std::endl;
 	return result;
 }
 
@@ -30,15 +28,18 @@ int wielomian(int a[], int n, int x)
 		{
 			partial *= x;
 			++counter;
+			std::cout << counter << " ";
 		}
 		partial *= a[i];
 		++counter;
+		std::cout << counter << " o ";
 		result += partial;
 	}
 	result += a[n];
 	return result;
 }
 
+//(n1(n1+1)/2 - 1) + (n2(n2+1)/2 - 1) + 1
 int iloczynWielomianow(int a[], int b[], int n1, int n2, int x)
 {
 	int result1 = wielomian(a, n1, x);
@@ -47,9 +48,19 @@ int iloczynWielomianow(int a[], int b[], int n1, int n2, int x)
 	return result1 * result2;
 }
 
-void iloczynMacierzy(int a[][2], int b[][2], int n)
+//n^3
+void iloczynMacierzy(int a[][3], int b[][3], int n)
 {
 	int m3[n][n];
+	
+	for(int i = 0; i < n; ++i)
+	{
+		for(int j = 0; j < n; ++j)
+        {
+            m3[i][j] = 0;
+        }
+	}
+	
 	for(int i = 0; i < n; ++i)
 	{
 		for(int j = 0; j < n; ++j)
@@ -57,6 +68,7 @@ void iloczynMacierzy(int a[][2], int b[][2], int n)
 			for(int k = 0; k < n; ++k)
 			{
 				m3[i][j] += a[i][k] * b[k][j];
+				++counter;
 			}
 		}
 	}
@@ -69,18 +81,62 @@ void iloczynMacierzy(int a[][2], int b[][2], int n)
 	}
 }
 
+int wyznacznikMacierzy(int a[3][3], int n)
+{
+	int wyzn = 0;
+	int b[3][3];
+	
+	if (n == 2)
+	{
+		++counter;
+		++counter;
+		return ((a[0][0] * a[1][1]) - (a[1][0] * a[0][1]));
+	}
+    else
+    {
+	for (int x = 0; x < n; x++) 
+	{
+	int subi = 0; 
+	for (int i = 1; i < n; i++) 
+	{
+		int subj = 0;
+		for (int j = 0; j < n; j++) 
+		{
+			if (j == x)
+			continue;
+			b[subi][subj] = a[i][j];
+			subj++;
+		}
+		subi++;
+		}
+
+		int pow = -1;
+		if(x % 2 == 0)
+		{
+			pow = 1;
+		}
+
+		wyzn = wyzn + pow * a[0][x] * wyznacznikMacierzy(b, n - 1);
+		++counter;
+		++counter;
+	}
+      	}
+	return wyzn;
+}
+
 int main()
 {
 	int n1 = 3;
 	int n2 = 4;
-	int n3 = 2;
+	int n3 = 3;
 	int x = 10;
 	int s1[n1] = {1,2,3};
 	int s2[n1] = {1,1,1};
 	int w1[n1+1] = {5,4,3,2};
 	int w2[n2+1] = {3,2,4,4,1};
-	int m1[2][2] = {{2, 5}, {1, -2}};
-	int m2[2][2] = {{3, -1}, {7, 4}};
+	int m1[3][3] = {{2, 5, 1}, {1, -2, 7}, {1, 5, 6}};
+	int m2[3][3] = {{3, -1, 6}, {7, 4, 2}, {1, 2, 3}};
+	int m3[3][3] = {{1, 2, 3}, {3, 2, 1}, {2, 1, 3}};
 	
 	std::cout << skalar(s1, s2, n1) << " Number of multiplications: " << counter << std::endl;
 	counter = 0;
@@ -88,7 +144,9 @@ int main()
 	counter = 0;
 	std::cout << iloczynWielomianow(w1, w2, n1, n2, x) << " Number of multiplications: " << counter << std::endl;
 	counter = 0;
-	
-	std::cout << m1[0] << m2[0] << std::endl;
 	iloczynMacierzy(m1, m2, n3);
+	std::cout << " Number of multiplications: " << counter << std::endl;
+	counter = 0;
+	std::cout << wyznacznikMacierzy(m3, n3) << std::endl;
+	std::cout << " Number of multiplications: " << counter << std::endl;
 }
