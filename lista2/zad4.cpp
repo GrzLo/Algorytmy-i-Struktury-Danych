8380a53bca87e874ec1b05eabb0d5ec3d5b26c93
GrzLo <251583@uwr.edu.pl>
2019-03-14 13:05:20 +0100

    Update zad4.cpp

--- lista2/zad4.cpp
+++ lista2/zad4.cpp
@@ -17,16 +17,25 @@ void reverse(int *tab, int begin, int end)
 
 void move(int *tab, int n, int k)
 {
-    reverse(tab, 0, n-1); //6 5 | 4 3 2 1
-    reverse(tab, 0, k-1); //5 6 | 4 3 2 1 
-    reverse(tab, k, n-1); //5 6 | 1 2 3 4
+	k = k % n;
+	if(k == 0)
+	{
+		
+	}
+	else
+	{
+		reverse(tab, 0, n-1); //6 5 | 4 3 2 1
+		reverse(tab, 0, k-1); //5 6 | 4 3 2 1 
+		reverse(tab, k, n-1); //5 6 | 1 2 3 4
+	}
 }
 
 int main()
 {
 	int n = 6;
+	int k = 13;
 	int a[n] = {1, 2, 3, 4, 5, 6};
-	move(a, n, 2);
+	move(a, n, k);
 	
 	for(int i = 0; i < n; ++i)
 	{
