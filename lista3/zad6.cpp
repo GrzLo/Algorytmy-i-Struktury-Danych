c49c619d8625d62583ed67980c7395500f89d2ea
GrzLo <251583@uwr.edu.pl>
2019-04-04 11:22:06 +0200

    Update zad6.cpp

--- lista3/zad6.cpp
+++ lista3/zad6.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+static int counter = 0;
+
 void maxSum(int a[], int n, int k) 
 {
     if (k > n) 
@@ -11,6 +13,7 @@ void maxSum(int a[], int n, int k)
     for (int i = 0; i < k; i++) 
     {
         sum += a[i];
+        ++counter;
     }
 
     int newSum = sum;
@@ -18,19 +21,23 @@ void maxSum(int a[], int n, int k)
     {
         newSum -= a[i-k];
         newSum += a[i];
+        counter+=2;
 
         if (newSum > sum) 
         {
             sum = newSum;
         }
     }
-    std::cout << sum;
+    std::cout << sum << std::endl;
 }
 
 int main() 
 {
 	int n = 10;
 	int k = 5;
-    int t[n] = {1, 20, 3, 40, 5, 60, 7, 80, 9, 0};
+    int t[n] = {1, 20, 3, 40, 5, 60, 7, 80, 9};
+    
+    //operacje 2(n-k) + k
     maxSum(t, n, k);
+    std::cout << counter << std::endl;
 }
