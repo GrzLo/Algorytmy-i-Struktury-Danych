e72376015f9c62909cf5cb4832857038c3be9b04
GrzLo <251583@uwr.edu.pl>
2019-04-04 11:22:49 +0200

    Update zad6.cpp

--- lista3/zad6.cpp
+++ lista3/zad6.cpp
@@ -35,9 +35,9 @@ int main()
 {
 	int n = 10;
 	int k = 5;
-    int t[n] = {1, 20, 3, 40, 5, 60, 7, 80, 9};
-    
-    //operacje 2(n-k) + k
-    maxSum(t, n, k);
-    std::cout << counter << std::endl;
+	int t[n] = {1, 20, 3, 40, 5, 60, 7, 80, 9};
+
+	//operacje 2(n-k) + k
+	maxSum(t, n, k);
+	std::cout << counter << std::endl;
 }
