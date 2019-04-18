ffe4d59850f0164d7b29dc90f03d3c61ecffb427
GrzLo <251583@uwr.edu.pl>
2019-04-18 14:38:48 +0200

    Update zad3.cpp

--- lista4/zad3.cpp
+++ lista4/zad3.cpp
@@ -1,25 +1,70 @@
+#include <iostream>
+#include <cmath>
 
-void countingSort(int tab[], int n, int exp)
-{
-    int output[n];
-    int i, count[10] = {0};
-
-    
-    for (i = 0; i < n; i++)
-        count[ (tab[i]/exp)%10 ]++;
-
-    
-    for (i = 1; i < 10; i++)
-        count[i] += count[i - 1];
-
-    
-    for (i = n - 1; i >= 0; i--)
+int k_th(int x, int k) {
+    int length = floor(log10(x)) + 1;
+    int result = 0;
+    for (int i = length; i != k; i--) 
     {
-        output[count[ (tab[i]/exp)%10 ] - 1] = tab[i];
-        count[ (tab[i]/exp)%10 ]--;
+        result = x % 10;
+        x = x / 10;
+    }
+    return result;
+}
+
+void countingSort(int t[], int n, int c)
+{
+    int max = t[0];
+    int min = t[0];
+
+    for (int i = 0; i < n; i++)
+    {
+        if (t[i] > max)
+            max = t[i];
+        if (t[i] < min)
+            min = t[i];
     }
 
-  
-    for (i = 0; i < n; i++)
-        tab[i] = output[i];
+    std::cout << "Min = " << min << ", max = " << max << std::endl;
+
+    int range = max - min + 1; 
+    int count[range] = {0};
+    
+    int i;
+    int out[n];
+    c = pow(10, c);
+    
+    for(i=0;i<n;i++)
+        ++count[(t[i]/c)%10];
+    
+    for(i=1;i<range;i++)
+        count[i]+=count[i-1];
+     
+    for(i=n-1;i>=0;i--){
+        out[count[(t[i]/c)%10]-1]=t[i];
+        --count[(t[i]/c)%10];
+    }
+    
+    for(i=0;i<n;i++)
+        t[i]=out[i];
+}
+
+void print(int t[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << t[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main()
+{
+    int array[10] = {51,12,23,94,15,26,27,58,29,61};
+    print(array, 10);
+    countingSort(array, 10, 0);
+    print(array, 10);
+    countingSort(array, 10, 1);
+    print(array, 10);
+
 }
